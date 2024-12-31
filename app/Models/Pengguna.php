195f5c54dcf3f2fd10603c8270af996da57cf8d3
Arxvzy <szaryabima@gmail.com>
2024-12-31 15:34:02 +0700

    refactor: include spatie HasRoles in model

--- app/Models/Pengguna.php
+++ app/Models/Pengguna.php
@@ -3,21 +3,22 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class Pengguna extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var list<string>
      */
-
+    
     protected $table = 'pengguna';
 
     protected $fillable = [
