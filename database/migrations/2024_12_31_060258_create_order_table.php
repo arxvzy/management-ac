7d45eaf611e5807379535393182754c215d00517
Arxvzy <szaryabima@gmail.com>
2024-12-31 13:17:29 +0700

    feat: create table order

---
+++ database/migrations/2024_12_31_060258_create_order_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_jasa');
+            $table->integer('id_pengguna');
+            $table->integer('id_pelanggan');
+            $table->date('jadwal');
+            $table->float('harga_awal');
+            $table->float('harga_akhir');
+            $table->dateTime('tgl_pengerjaan');
+            $table->string('status');
+            $table->text('testimoni');     
+            $table->timestamps();
+
+            $table->foreign('id_jasa')->references('id')->on('jasa');
+            $table->foreign('id_pengguna')->references('id')->on('pengguna');
+            $table->foreign('id_pelanggan')->references('id')->on('pelanggan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order');
+    }
+};
