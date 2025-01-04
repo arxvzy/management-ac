ea9fd828fc2c13b2402e13e21901baa9f73c5432
Arxvzy <szaryabima@gmail.com>
2025-01-04 16:33:27 +0700

    fix: delete unused file

--- resources/views/admin/tambah.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.admin')
-@section('content')
-    <div class="container px-6 mx-auto grid">
-        <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-            Tambah Pengguna
-        </h2>
-    </div>
-@endsection
