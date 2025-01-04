760f216b30750292293769b444cc86fbeb450151
Arxvzy <szaryabima@gmail.com>
2025-01-04 16:37:06 +0700

    feat: add route to post user

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,9 @@ use Illuminate\Support\Facades\Route;
 // Route::group(['prefix' => 'dashboard'], function () {
     //single action controllers
     Route::get('/', function () { return view('admin.index'); })->name('admin.home');
-    Route::get('/pengguna', [PenggunaController::class, 'index'])->name('admin.pengguna');
+    Route::get('/pengguna', [PenggunaController::class, 'index'])->name('admin.pengguna.index');
     Route::get('/pengguna/tambah', [PenggunaController::class, 'create'])->name('admin.pengguna.tambah');
+    Route::post('/pengguna', [PenggunaController::class, 'store'])->name('admin.pengguna.simpan');
 
-    Route::group(['prefix' => 'pages', 'as' => 'page.'], function () {
-        Route::view('/404-page', 'admin.pages.404')->name('404');
-        Route::view('/blank-page', 'admin.pages.blank')->name('blank');
-        Route::view('/create-account-page', 'admin.pages.create-account')->name('create-account');
-        Route::view('/forgot-password-page', 'admin.pages.forgot-password')->name('forgot-password');
-        Route::view('/login-page', 'admin.pages.login')->name('login');
-    });
 // });
 
