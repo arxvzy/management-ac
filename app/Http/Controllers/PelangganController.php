6507968ab64eb7a41f6912be8f2971af740c327a
Arxvzy <szaryabima@gmail.com>
2025-01-07 11:57:45 +0700

    feat: create pelanggan controller

---
+++ app/Http/Controllers/PelangganController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pelanggan;
+use Illuminate\Http\Request;
+
+class PelangganController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $pelanggan = Pelanggan::all();
+        return view('admin.pelanggan.index', compact('pelanggan'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.pelanggan.tambah');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+        'nama' => 'required|string|max:255', 
+        'alamat' => 'required',
+        'no_hp' => 'required',
+        'koordinat' => 'required'
+        ]);
+
+        $pelanggan = Pelanggan::create($validated);
+        return redirect()->route('admin.pelanggan.index');
+    }
+
+
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Pelanggan $pelanggan)
+    {
+        return view('admin.pelanggan.edit', compact('pelanggan'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Pelanggan $pelanggan)
+    {
+        $validated = $request->validate([
+        'nama' => 'required|string|max:255', 
+        'alamat' => 'required',
+        'no_hp' => 'required',
+        'koordinat' => 'required'
+        ]);
+
+        $pelanggan->update($validated);
+        return redirect()->route('admin.pelanggan.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pelanggan $pelanggan)
+    {
+        $pelanggan->delete();
+        return redirect()->route('admin.pelanggan.index');
+    }
+}
