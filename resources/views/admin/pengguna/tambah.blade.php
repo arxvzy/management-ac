50ceab364223623cd28bfa8dbcb9ca861cde06ee
Arxvzy <szaryabima@gmail.com>
2025-01-07 11:40:23 +0700

    refactor: change role pengguna to teknisi

--- resources/views/admin/pengguna/tambah.blade.php
+++ resources/views/admin/pengguna/tambah.blade.php
@@ -53,8 +53,8 @@
                     <label class="inline-flex items-center ml-6 text-gray-600 dark:text-gray-400">
                         <input type="radio"
                             class="text-purple-600 form-radio focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray"
-                            name="role" value="pengguna" />
-                        <span class="ml-2">Pengguna</span>
+                            name="role" value="teknisi" />
+                        <span class="ml-2">Teknisi</span>
                     </label>
                 </div>
                 @error('role')
