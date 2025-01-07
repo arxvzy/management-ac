0e5bb1fb69a59f29bfa77c1483e90f4b273366c6
Arxvzy <szaryabima@gmail.com>
2025-01-07 11:40:50 +0700

    feat: add routes to delete and edit user

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,12 @@ use Illuminate\Support\Facades\Route;
 // Route::group(['prefix' => 'dashboard'], function () {
     //single action controllers
     Route::get('/', function () { return view('admin.index'); })->name('admin.home');
+
     Route::get('/pengguna', [PenggunaController::class, 'index'])->name('admin.pengguna.index');
     Route::get('/pengguna/tambah', [PenggunaController::class, 'create'])->name('admin.pengguna.tambah');
     Route::post('/pengguna', [PenggunaController::class, 'store'])->name('admin.pengguna.simpan');
-
+    Route::delete('/pengguna/{id}', [PenggunaController::class, 'destroy'])->name('admin.pengguna.hapus');
+    Route::get('/pengguna/{id}/edit', [PenggunaController::class, 'edit'])->name('admin.pengguna.edit');
+    Route::put('/pengguna/{id}', [PenggunaController::class, 'update'])->name('admin.pengguna.update');
 // });
 
