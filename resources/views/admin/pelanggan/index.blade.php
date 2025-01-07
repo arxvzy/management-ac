6041f8fa47fefc94974b44477b597e50e811ad39
Arxvzy <szaryabima@gmail.com>
2025-01-07 11:58:58 +0700

    feat: create base page of pelanggan

---
+++ resources/views/admin/pelanggan/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin')
+@section('title', 'Pelanggan')
+@section('content')
+    <div class="container px-6 mx-auto grid">
+        <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+            Pelanggan
+        </h2>
+
+
+    </div>
+@endsection
