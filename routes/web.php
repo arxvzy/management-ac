1bc26cd78ab491081327e91a939011010d4acef5
Arxvzy <szaryabima@gmail.com>
2025-01-07 11:59:54 +0700

    feat: add pelanggan index route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\PenggunaController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PenggunaController;
+use App\Http\Controllers\PelangganController;
 
 // Route::group(['prefix' => 'dashboard'], function () {
     //single action controllers
@@ -13,5 +14,7 @@ use Illuminate\Support\Facades\Route;
     Route::delete('/pengguna/{id}', [PenggunaController::class, 'destroy'])->name('admin.pengguna.hapus');
     Route::get('/pengguna/{id}/edit', [PenggunaController::class, 'edit'])->name('admin.pengguna.edit');
     Route::put('/pengguna/{id}', [PenggunaController::class, 'update'])->name('admin.pengguna.update');
+
+    Route::get('/pelanggan', [PelangganController::class, 'index'])->name('admin.pelanggan.index');
 // });
 
