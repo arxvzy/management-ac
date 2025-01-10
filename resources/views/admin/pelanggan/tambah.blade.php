faebbfc42266ccf8be290d6d1907a46a0786bb67
Arxvzy <szaryabima@gmail.com>
2025-01-10 22:01:20 +0700

    feat: create add pelanggan page

---
+++ resources/views/admin/pelanggan/tambah.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.admin')
+@section('title', 'Tambah Pelanggan')
+
+@section('content')
+    <div>
+        <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+            Tambah Pengguna
+        </h2>
+
+
+        <form action="{{ route('admin.pelanggan.simpan') }}" method="POST"
+            class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
+            @csrf
+            <label class="block text-sm">
+                <span class="text-gray-700 dark:text-gray-400">Nama</span>
+                <input
+                    class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                    placeholder="Nama" name="nama" type="text" value="{{ old('nama') }}" />
+                @error('nama')
+                    <span class="text-red-400">{{ $message }}</span>
+                @enderror
+            </label>
+            <label class="mt-4 block text-sm">
+                <span class="text-gray-700 dark:text-gray-400">No. Telepon</span>
+                <input
+                    class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                    placeholder="No. Telp" name="no_hp" type="tel" value="{{ old('no_hp') }}" />
+                @error('no_hp')
+                    <span class="text-red-400">{{ $message }}</span>
+                @enderror
+            </label>
+            <label class="mt-4 block text-sm">
+                <span class="text-gray-700 dark:text-gray-400">Alamat</span>
+                <input
+                    class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                    placeholder="Alamat" name="alamat" type="text" value="{{ old('alamat') }}" />
+                @error('password')
+                    <span class="text-red-400">{{ $message }}</span>
+                @enderror
+            </label>
+            <label class="mt-4 block text-sm">
+                <span class="text-gray-700 dark:text-gray-400">Koordinat</span>
+                <input
+                    class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                    placeholder="https://maps.app.goo.gl/..." name="koordinat" type="text"
+                    value="{{ old('koordinat') }}" />
+                @error('koordinat')
+                    <span class="text-red-400">{{ $message }}</span>
+                @enderror
+            </label>
+            <x-button class="mt-6 py-3" type="submit">Simpan</x-button>
+    </div>
+    </form>
+    </div>
+@endsection
