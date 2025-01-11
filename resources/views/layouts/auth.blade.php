aee84befdd3aaafdf6657f4692706dde4d54ed46
Arxvzy <szaryabima@gmail.com>
2025-01-11 21:03:38 +0700

    feat: create layout for auth

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full bg-white">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title')</title>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="h-full">
+    @yield('content')
+</body>
+
+</html>
