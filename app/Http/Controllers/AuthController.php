9f06329f277cbe06bc6efa35124f4cb8d544240e
Arxvzy <szaryabima@gmail.com>
2025-01-11 21:01:03 +0700

    feat: create auth controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pengguna;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'username' => ['required'],
+            'password' => ['required']
+        ]);
+
+        $remember = false;
+
+        if($request->has('remember')) {
+            $remember = true;
+        }
+
+        $credentials['is_active'] = true;
+        if (Auth::attempt($credentials, $remember)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/');
+        }
+
+        return back()->with('error', 'Username atau password salah.');
+    }
+}
