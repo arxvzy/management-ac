29fdf1d9248082e4c7d20712994f81a4983595f0
Arxvzy <szaryabima@gmail.com>
2025-01-11 14:37:18 +0700

    refactor: change input type to url in koordinat

--- resources/views/admin/pelanggan/tambah.blade.php
+++ resources/views/admin/pelanggan/tambah.blade.php
@@ -42,7 +42,7 @@
                 <span class="text-gray-700 dark:text-gray-400">Koordinat</span>
                 <input
                     class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
-                    placeholder="https://maps.app.goo.gl/..." name="koordinat" type="text"
+                    placeholder="https://maps.app.goo.gl/..." name="koordinat" type="url"
                     value="{{ old('koordinat') }}" />
                 @error('koordinat')
                     <span class="text-red-400">{{ $message }}</span>
