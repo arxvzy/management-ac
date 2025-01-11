8d17ef067e7293b9893b91f1206d78bb2f32b0fb
Arxvzy <szaryabima@gmail.com>
2025-01-11 14:40:00 +0700

    feat: add menu pengeluaran in sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -17,6 +17,17 @@
                 </a>
             </li>
             <li class="relative px-6 py-3">
+                @if (request()->routeIs('admin.pengeluaran.*'))
+                    <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                        aria-hidden="true"></span>
+                @endif
+                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                    href="{{ route('admin.pengeluaran.index') }}">
+                    <x-heroicon-o-shopping-cart class="w-5 h-5" />
+                    <span class="ml-4">Kelola Pengeluaran</span>
+                </a>
+            </li>
+            <li class="relative px-6 py-3">
                 @if (request()->routeIs('admin.pelanggan.*'))
                     <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
                         aria-hidden="true"></span>
@@ -24,7 +35,7 @@
                 <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                     href="{{ route('admin.pelanggan.index') }}">
                     <x-heroicon-o-users class="w-5 h-5" />
-                    <span class="ml-4">Pelanggan</span>
+                    <span class="ml-4">Kelola Pelanggan</span>
                 </a>
             </li>
             <li class="relative px-6 py-3">
@@ -35,7 +46,7 @@
                 <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                     href="{{ route('admin.pengguna.index') }}">
                     <x-heroicon-o-user-circle class="w-5 h-5" />
-                    <span class="ml-4">Pengguna</span>
+                    <span class="ml-4">Kelola Pengguna</span>
                 </a>
             </li>
         </ul>
@@ -71,6 +82,17 @@
                 </a>
             </li>
             <li class="relative px-6 py-3">
+                @if (request()->routeIs('admin.pengeluaran.*'))
+                    <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                        aria-hidden="true"></span>
+                @endif
+                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                    href="{{ route('admin.pengeluaran.index') }}">
+                    <x-heroicon-o-shopping-cart class="w-5 h-5" />
+                    <span class="ml-4">Kelola Pengeluaran</span>
+                </a>
+            </li>
+            <li class="relative px-6 py-3">
                 @if (request()->routeIs('admin.pelanggan.*'))
                     <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
                         aria-hidden="true"></span>
@@ -78,7 +100,7 @@
                 <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                     href="{{ route('admin.pelanggan.index') }}">
                     <x-heroicon-o-users class="w-5 h-5" />
-                    <span class="ml-4">Pelanggan</span>
+                    <span class="ml-4">Kelola Pelanggan</span>
                 </a>
             </li>
             <li class="relative px-6 py-3">
@@ -89,7 +111,7 @@
                 <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                     href="{{ route('admin.pengguna.index') }}">
                     <x-heroicon-o-user-circle class="w-5 h-5" />
-                    <span class="ml-4">Pengguna</span>
+                    <span class="ml-4">Kelola Pengguna</span>
                 </a>
             </li>
         </ul>
