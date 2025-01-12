47e58584dfb17480498aaa6dd95234badfc2964e
Arxvzy <szaryabima@gmail.com>
2025-01-12 21:00:15 +0700

    feat: create pelanggan factory

---
+++ database/factories/PelangganFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Pelanggan;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Pelanggan>
+ */
+class PelangganFactory extends Factory
+{
+    protected $model = Pelanggan::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nama' => $this->faker->name,
+            'no_hp' => $this->faker->phoneNumber,
+            'alamat' => $this->faker->address,
+            'koordinat' => "https://maps.app.goo.gl/...",
+        ];
+    }
+}
