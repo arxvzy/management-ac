8df912dde28e8fb1c0ff0b6e70809dd5197c451b
Arxvzy <szaryabima@gmail.com>
2025-01-12 15:05:35 +0700

    feat: create add jasa page

---
+++ resources/views/admin/jasa/tambah.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.admin')
+@section('title', 'Tambah Jasa')
+
+@section('content')
+    <div>
+        <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+            Tambah Jasa
+        </h2>
+
+
+        <form action="{{ route('admin.jasa.simpan') }}" method="POST"
+            class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
+            @csrf
+            <label class="block text-sm">
+                <span class="text-gray-700 dark:text-gray-400">Jasa</span>
+                <input
+                    class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                    placeholder="masukkan jasa" name="jasa" type="text" value="{{ old('jasa') }}" />
+                @error('jasa')
+                    <span class="text-red-400">{{ $message }}</span>
+                @enderror
+            </label>
+            <label class="block text-sm">
+                <span class="text-gray-700 dark:text-gray-400">Keterangan</span>
+                <input
+                    class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                    placeholder="keterangan" name="keterangan" type="text" value="{{ old('keterangan') }}" />
+                @error('keterangan')
+                    <span class="text-red-400">{{ $message }}</span>
+                @enderror
+            </label>
+            <x-button class="mt-6 py-3" type="submit">Simpan</x-button>
+    </div>
+    </form>
+    </div>
+@endsection
