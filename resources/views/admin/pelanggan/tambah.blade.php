804ac8817bf9e47f4729fa48e0f48dbc836e7b31
Arxvzy <szaryabima@gmail.com>
2025-01-12 15:06:44 +0700

    refactor: rename header

--- resources/views/admin/pelanggan/tambah.blade.php
+++ resources/views/admin/pelanggan/tambah.blade.php
@@ -4,7 +4,7 @@
 @section('content')
     <div>
         <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-            Tambah Pengguna
+            Tambah Pelanggan
         </h2>
 
 
