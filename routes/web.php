b4ce28c134b5239e95ec51d860fd00a56832fa8e
Arxvzy <szaryabima@gmail.com>
2025-01-12 15:08:13 +0700

    refactor: add jasa route, rename route parameter for some controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\JasaController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\PenggunaController;
 use App\Http\Controllers\PelangganController;
 use App\Http\Controllers\PengeluaranController;
@@ -17,21 +19,30 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/pengguna', [PenggunaController::class, 'index'])->name('admin.pengguna.index');
     Route::get('/pengguna/tambah', [PenggunaController::class, 'create'])->name('admin.pengguna.tambah');
     Route::post('/pengguna', [PenggunaController::class, 'store'])->name('admin.pengguna.simpan');
-    Route::delete('/pengguna/{id}', [PenggunaController::class, 'destroy'])->name('admin.pengguna.hapus');
-    Route::get('/pengguna/{id}/edit', [PenggunaController::class, 'edit'])->name('admin.pengguna.edit');
-    Route::put('/pengguna/{id}', [PenggunaController::class, 'update'])->name('admin.pengguna.update');
+    Route::delete('/pengguna/{pengguna}', [PenggunaController::class, 'destroy'])->name('admin.pengguna.hapus');
+    Route::get('/pengguna/{pengguna}/edit', [PenggunaController::class, 'edit'])->name('admin.pengguna.edit');
+    Route::put('/pengguna/{pengguna}', [PenggunaController::class, 'update'])->name('admin.pengguna.update');
 
     Route::get('/pelanggan', [PelangganController::class, 'index'])->name('admin.pelanggan.index');
     Route::get('/pelanggan/tambah', [PelangganController::class, 'create'])->name('admin.pelanggan.tambah');
     Route::post('/pelanggan', [PelangganController::class, 'store'])->name('admin.pelanggan.simpan');
     Route::get('/pelanggan/{pelanggan}/edit', [PelangganController::class, 'edit'])->name('admin.pelanggan.edit');
-    Route::delete('/pelanggan/{id}', [PelangganController::class, 'destroy'])->name('admin.pelanggan.hapus');
-    Route::put('/pelanggan/{id}', [PelangganController::class, 'update'])->name('admin.pelanggan.update');
+    Route::delete('/pelanggan/{pelanggan}', [PelangganController::class, 'destroy'])->name('admin.pelanggan.hapus');
+    Route::put('/pelanggan/{pelanggan}', [PelangganController::class, 'update'])->name('admin.pelanggan.update');
 
     Route::get('/pengeluaran', [PengeluaranController::class, 'index'])->name('admin.pengeluaran.index');
     Route::get('/pengeluaran/tambah', [PengeluaranController::class, 'create'])->name('admin.pengeluaran.tambah');
     Route::post('/pengeluaran', [PengeluaranController::class, 'store'])->name('admin.pengeluaran.simpan');
     Route::get('/pengeluaran/{pengeluaran}/edit', [PengeluaranController::class, 'edit'])->name('admin.pengeluaran.edit');
-    Route::delete('/pengeluaran/{id}', [PengeluaranController::class, 'destroy'])->name('admin.pengeluaran.hapus');
-    Route::put('/pengeluaran/{id}', [PengeluaranController::class, 'update'])->name('admin.pengeluaran.update');
+    Route::delete('/pengeluaran/{pengeluaran}', [PengeluaranController::class, 'destroy'])->name('admin.pengeluaran.hapus');
+    Route::put('/pengeluaran/{pengeluaran}', [PengeluaranController::class, 'update'])->name('admin.pengeluaran.update');
+
+    Route::get('/jasa', [JasaController::class, 'index'])->name('admin.jasa.index');
+    Route::get('/jasa/tambah', [JasaController::class, 'create'])->name('admin.jasa.tambah');
+    Route::post('/jasa', [JasaController::class, 'store'])->name('admin.jasa.simpan');
+    Route::get('/jasa/{jasa}/edit', [JasaController::class, 'edit'])->name('admin.jasa.edit');
+    Route::delete('/jasa/{jasa}', [JasaController::class, 'destroy'])->name('admin.jasa.hapus');
+    Route::put('/jasa/{jasa}', [JasaController::class, 'update'])->name('admin.jasa.update');
+
+    Route::get('/order', [OrderController::class, 'index'])->name('admin.order.index');
 });
