a0bbc313b68a69f5baaa9f5830e0e7069f4c70e3
Arxvzy <szaryabima@gmail.com>
2025-01-12 20:59:51 +0700

    feat: create jasa factory

---
+++ database/factories/JasaFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Jasa;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Jasa>
+ */
+class JasaFactory extends Factory
+{
+    protected $model = Jasa::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'jasa' => $this->faker->randomElement(['Premium', 'Basic', 'Minimum']),
+            'keterangan' => $this->faker->sentence,
+        ];
+    }
+}
