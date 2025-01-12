195f8508b1a61de74d1ff3c70f1ac51464abfd9b
Arxvzy <szaryabima@gmail.com>
2025-01-12 15:06:00 +0700

    feat: create order index page

---
+++ resources/views/admin/order/index.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.admin')
+@section('title', 'Manajemen Order')
+@section('content')
+    <div>
+        <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+            Manajemen Order
+        </h2>
+        @if (session()->has('success'))
+            <x-alert>Order</x-alert>
+        @endif
+        <h4 class="mt-4 text-lg font-semibold">
+            <x-button class="py-2" href="{{ route('admin.order.tambah') }}">Tambah</x-button>
+        </h4>
+
+        <table id="orderTable" class="display">
+            <thead>
+                <tr>
+                    <th>Jasa</th>
+                    <th>Pelanggan</th>
+                    <th>Harga Awal</th>
+                    <th>Harga Akhir</th>
+                    <th>Teknisi</th>
+                    <th>Metode Pembayaran</th>
+                    <th>Jadwal</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($orders as $order)
+                    <tr>
+                        <td>{{ $order->jasa->jasa }}</td>
+                        <td>{{ $order->pelanggan->nama }}</td>
+                        <td>{{ $order->harga_awal }}</td>
+                        <td>{{ $order->harga_akhir }}</td>
+                        <td>{{ $order->pengguna->nama }}</td>
+                        <td>{{ $order->metode_pembayaran }}</td>
+                        <td>{{ $order->jadwal }}</td>
+                        <td>
+                            <div class="flex items-center space-x-4 text-sm">
+                                <a href="{{ route('admin.order.edit', $order->id) }}"
+                                    class="flex items-center justify-start px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray "
+                                    aria-label="Edit">
+                                    <x-heroicon-s-pencil class="w-5 h-5" />
+                                </a>
+                                <form action="{{ route('admin.order.hapus', $order->id) }}" method="POST"
+                                    style="display: none;" id="delete-form-{{ $order->id }}">
+                                    @csrf
+                                    @method('DELETE')
+                                </form>
+                                <button
+                                    onclick="event.preventDefault(); confirmDelete({{ $order->id }}, '{{ $order->pelanggan->nama }}', 'order');"
+                                    class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-red-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray">
+                                    <x-heroicon-s-trash class="w-5 h-5" />
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <script>
+        $(document).ready(function() {
+            $('#orderTable').DataTable({
+                info: false,
+                paging: false,
+                responsive: true,
+                columnDefs: [{
+                    targets: [4, 3, 1],
+                    orderable: false,
+                }, ],
+            });
+        });
+    </script>
+@endsection
