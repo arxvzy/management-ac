552a80aebb8de1b49ca3b9fdbe5b7b91e60c189a
Arxvzy <szaryabima@gmail.com>
2025-01-13 22:33:25 +0700

    style: add space between inputs

--- resources/views/admin/jasa/tambah.blade.php
+++ resources/views/admin/jasa/tambah.blade.php
@@ -20,7 +20,7 @@
                     <span class="text-red-400">{{ $message }}</span>
                 @enderror
             </label>
-            <label class="block text-sm">
+            <label class="block mt-4 text-sm">
                 <span class="text-gray-700 dark:text-gray-400">Keterangan</span>
                 <input
                     class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
