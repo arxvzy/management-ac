8bd1666febaa420e7043aa8ecb6bb52a4f73d2b7
Arxvzy <szaryabima@gmail.com>
2025-01-13 22:40:12 +0700

    refactor: add penugasan to menu sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -17,6 +17,17 @@
                 </a>
             </li>
             <li class="relative px-6 py-3">
+                @if (request()->routeIs('admin.penugasan.*'))
+                    <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                        aria-hidden="true"></span>
+                @endif
+                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                    href="{{ route('admin.penugasan.index') }}">
+                    <x-heroicon-o-document-text class="w-5 h-5" />
+                    <span class="ml-4">Penugasan</span>
+                </a>
+            </li>
+            <li class="relative px-6 py-3">
                 @if (request()->routeIs('admin.jasa.*'))
                     <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
                         aria-hidden="true"></span>
@@ -101,6 +112,17 @@
                     href="{{ route('admin.home') }}">
                     <x-heroicon-o-home class="w-5 h-5" />
                     <span class="ml-4">Dashboard</span>
+                </a>
+            </li>
+            <li class="relative px-6 py-3">
+                @if (request()->routeIs('admin.penugasan.*'))
+                    <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                        aria-hidden="true"></span>
+                @endif
+                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                    href="{{ route('admin.penugasan.index') }}">
+                    <x-heroicon-o-document-text class="w-5 h-5" />
+                    <span class="ml-4">Penugasan</span>
                 </a>
             </li>
             <li class="relative px-6 py-3">
