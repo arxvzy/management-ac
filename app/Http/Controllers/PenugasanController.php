728e08193748cdbb3b065b19c53b6db5cb9f68d5
Arxvzy <szaryabima@gmail.com>
2025-01-13 22:31:30 +0700

    feat: create penugasan controller

---
+++ app/Http/Controllers/PenugasanController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Jasa;
+use App\Models\Order;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PenugasanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $user = Auth::user();
+        if ($user->role == 'teknisi') {
+            $orders = Order::query()
+                ->orderByRaw("CASE 
+        WHEN status IS NULL OR status = '' THEN 0 
+        WHEN status = 'Selesai' THEN 1 
+        WHEN status = 'Batal' THEN 2 
+    END")->where('id_pengguna', $user->id)->get();
+        } else {
+            $orders = Order::query()
+                ->orderByRaw("CASE 
+        WHEN status IS NULL OR status = '' THEN 0 
+        WHEN status = 'Selesai' THEN 1 
+        WHEN status = 'Batal' THEN 2 
+    END")
+                ->get();
+        }
+        return view('admin.penugasan.index', compact('orders'));
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Order $order)
+    {
+        return view('admin.penugasan.edit', compact('order'));
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Order $order)
+    {
+        if ($request->has('deskripsi')) {
+            $order['deskripsi'] = $request->deskripsi;
+        }
+        if ($request->has('harga_akhir')) {
+            $order['harga_akhir'] = $request->harga_akhir;
+        }
+        if ($request->has('metode_pembayaran')) {
+            $order['metode_pembayaran'] = $request->metode_pembayaran;
+        }
+        if ($request->has('status')) {
+            $order['status'] = $request->status;
+        }
+        $order->save();
+        return redirect()->route('admin.penugasan.index');
+    }
+}
