7e81694dc41c24fb31863da46ff284a8ffe0fc0b
Arxvzy <szaryabima@gmail.com>
2025-01-13 22:28:47 +0700

    feat: create logout function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -34,4 +34,12 @@ class AuthController extends Controller
 
         return back()->with('error', 'Username atau password salah.');
     }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
+    }
 }
