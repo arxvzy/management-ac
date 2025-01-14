9a19daf7e4459bfb5bf6aae9e2cd0a54a7d624f9
Arxvzy <szaryabima@gmail.com>
2025-01-14 21:10:37 +0700

    refactor: add service reminder menu to sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -28,6 +28,17 @@
                 </a>
             </li>
             <li class="relative px-6 py-3">
+                @if (request()->routeIs('admin.pengingat.*'))
+                    <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                        aria-hidden="true"></span>
+                @endif
+                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                    href="{{ route('admin.pengingat.index') }}">
+                    <x-heroicon-o-calendar-date-range class="w-5 h-5" />
+                    <span class="ml-4">Pengingat Service</span>
+                </a>
+            </li>
+            <li class="relative px-6 py-3">
                 @if (request()->routeIs('admin.jasa.*'))
                     <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
                         aria-hidden="true"></span>
@@ -123,6 +134,17 @@
                     href="{{ route('admin.penugasan.index') }}">
                     <x-heroicon-o-document-text class="w-5 h-5" />
                     <span class="ml-4">Penugasan</span>
+                </a>
+            </li>
+            <li class="relative px-6 py-3">
+                @if (request()->routeIs('admin.pengingat.*'))
+                    <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                        aria-hidden="true"></span>
+                @endif
+                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                    href="{{ route('admin.pengingat.index') }}">
+                    <x-heroicon-o-calendar-date-range class="w-5 h-5" />
+                    <span class="ml-4">Pengingat Service</span>
                 </a>
             </li>
             <li class="relative px-6 py-3">
