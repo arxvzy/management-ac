d9b09d2a2753487880f6dbe37388ee40ba73e5ae
Arxvzy <szaryabima@gmail.com>
2025-01-14 21:07:53 +0700

    refactor: change date range in order factory

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -29,7 +29,7 @@ class OrderFactory extends Factory
             'metode_pembayaran' => $this->faker->randomElement(['Tunai', 'Transfer']),
             'harga_awal' => $this->faker->numberBetween(10000, 50000),
             'harga_akhir' => $this->faker->numberBetween(50000, 100000),
-            'tgl_pengerjaan' => $this->faker->optional()->dateTimeBetween('+2 days', '+1 months'),
+            'tgl_pengerjaan' => $this->faker->optional()->dateTimeBetween('-100 days', '-80 days'),
             'status' => $this->faker->randomElement(['Selesai', 'Batal']),
             'testimoni' => $this->faker->optional()->sentence,
             'deskripsi' => $this->faker->sentence()
