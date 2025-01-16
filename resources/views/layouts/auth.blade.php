1b6b9ffbf121f96b161b1e4e29f49c9150f6d09e
Arxvzy <szaryabima@gmail.com>
2025-01-16 21:20:58 +0700

    style: small change

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -10,7 +10,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="h-full">
+<body class="min-h-screen flex items-center justify-center p-4">
     @yield('content')
 </body>
 
