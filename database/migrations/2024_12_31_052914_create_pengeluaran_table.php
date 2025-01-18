a558986229b44cc7653eca7b5c266f16db749ca1
Arxvzy <szaryabima@gmail.com>
2025-01-19 00:35:57 +0700

    refactor: make column null when parent deleted

--- database/migrations/2024_12_31_052914_create_pengeluaran_table.php
+++ database/migrations/2024_12_31_052914_create_pengeluaran_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
             $table->text('keterangan')->nullable();
             $table->float('nominal');
             $table->dateTime('tgl_pembelian');
-            $table->unsignedBigInteger('id_pengguna');
-            $table->foreign('id_pengguna')->references('id')->on('pengguna');
+            $table->unsignedBigInteger('id_pengguna')->nullable();
+            $table->foreign('id_pengguna')->references('id')->on('pengguna')->onDelete('set null');
             $table->timestamps();
         });
     }
