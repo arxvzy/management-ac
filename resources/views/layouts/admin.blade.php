4aba2e7e2a7afff8019163ff45c543a51e0f7488
Arxvzy <szaryabima@gmail.com>
2025-01-18 19:38:35 +0700

    refactor: remove unused lines

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html :class="{ 'theme-dark': dark }" x-data="data()" lang="en">
+<html :class="dark ? 'dark' : ''" x-data="data()" lang="en">
 
 <head>
     <meta charset="UTF-8" />
@@ -7,10 +7,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="/assets/css/tailwind.output.css" />
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <script src="/assets/js/utils.js" defer></script>
     <link rel="stylesheet" href="/assets/css/datatables.min.css" />
     <script src="/assets/js/datatables.min.js"></script>
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.4.1/css/responsive.dataTables.min.css" />
@@ -31,8 +29,8 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="/assets/js/init-alpine.js"></script>
-    <script src="/assets/js/focus-trap.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.4.1/js/dataTables.responsive.min.js"></script>
+    <script src="/assets/js/utils.js" defer></script>
 
 </body>
 
