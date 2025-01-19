798384f1040b0d24e2d2e6e6ae665a3dfa0b075a
Arxvzy <szaryabima@gmail.com>
2025-01-19 22:37:33 +0700

    refactor: teknisi cannot edit or delete records

--- resources/views/admin/pengeluaran/index.blade.php
+++ resources/views/admin/pengeluaran/index.blade.php
@@ -24,7 +24,9 @@
                     <th>Keterangan</th>
                     <th>Nominal</th>
                     <th>Tanggal Pembelian</th>
-                    <th></th>
+                    @if (auth()->user()->role == 'admin')
+                        <th></th>
+                    @endif
                 </tr>
             </thead>
             <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
@@ -37,25 +39,27 @@
                         <td data-order="{{ \Carbon\Carbon::parse($pengeluaran->tgl_pembelian)->format('Y-m-d H:i:s') }}">
                             {{ \Carbon\Carbon::parse($pengeluaran->tgl_pembelian)->translatedFormat('l, j F Y H:i') }}
                         </td>
-                        <td>
-                            <div class="flex items-center space-x-4 text-sm">
-                                <a href="{{ route('admin.pengeluaran.edit', $pengeluaran->id) }}"
-                                    class="flex items-center justify-start px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray "
-                                    aria-label="Edit">
-                                    <x-heroicon-s-pencil class="w-5 h-5" />
-                                </a>
-                                <form action="{{ route('admin.pengeluaran.hapus', $pengeluaran->id) }}" method="POST"
-                                    style="display: none;" id="delete-form-{{ $pengeluaran->id }}">
-                                    @csrf
-                                    @method('DELETE')
-                                </form>
-                                <button
-                                    onclick="event.preventDefault(); confirmDelete({{ $pengeluaran->id }}, '{{ $pengeluaran->keterangan }}', 'pengeluaran');"
-                                    class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-red-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray">
-                                    <x-heroicon-s-trash class="w-5 h-5" />
-                                </button>
-                            </div>
-                        </td>
+                        @if (auth()->user()->role == 'admin')
+                            <td>
+                                <div class="flex items-center space-x-4 text-sm">
+                                    <a href="{{ route('admin.pengeluaran.edit', $pengeluaran->id) }}"
+                                        class="flex items-center justify-start px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray "
+                                        aria-label="Edit">
+                                        <x-heroicon-s-pencil class="w-5 h-5" />
+                                    </a>
+                                    <form action="{{ route('admin.pengeluaran.hapus', $pengeluaran->id) }}" method="POST"
+                                        style="display: none;" id="delete-form-{{ $pengeluaran->id }}">
+                                        @csrf
+                                        @method('DELETE')
+                                    </form>
+                                    <button
+                                        onclick="event.preventDefault(); confirmDelete({{ $pengeluaran->id }}, '{{ $pengeluaran->keterangan }}', 'pengeluaran');"
+                                        class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-red-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray">
+                                        <x-heroicon-s-trash class="w-5 h-5" />
+                                    </button>
+                                </div>
+                            </td>
+                        @endif
                     </tr>
                 @endforeach
             </tbody>
@@ -73,7 +77,11 @@
                 searching: false,
                 order: [],
                 columnDefs: [{
-                    targets: [5],
+                    targets: [
+                        @if (auth()->user()->role == 'admin')
+                            5
+                        @endif
+                    ],
                     orderable: false,
                 }, ],
             });
