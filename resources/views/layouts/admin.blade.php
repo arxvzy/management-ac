5eb4164d90762bdd40fbe2f972438b4858bde0c6
Arya Bima <szaryabima@gmail.com>
2025-12-28 11:40:36 +0700

    style: favicon pt2

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>@yield('title')</title>
-        <link rel="shortcut icon" href="{{ asset('assets/img/logo.png') }}" type="image/x-icon">
+        <link rel="shortcut icon" href="{{ asset('assets/img/logo.ico') }}" type="image/x-icon">
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
             rel="stylesheet" />
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
